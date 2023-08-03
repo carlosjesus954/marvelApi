b776c3ffd6cbd1deedc1a9d9d8cd6cd78875982a
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-08-03 19:28:13 +0200

    Fixeando problemas

--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -61,17 +61,16 @@ export const NavBar = () => {
             />
 
             {errors.search && <p className="Header-errors">{errors.search}</p>}
-
-            <div className="Header-subcontainer" onClick={changeComicActivate}>
-              <span
-                className={`Header-span ${
-                  comicActivate ? "Header-span--activate" : ""
-                }`}
-              >
-                Buscar comics
-              </span>
-            </div>
           </form>
+          <div className="Header-subcontainer" onClick={changeComicActivate}>
+            <span
+              className={`Header-span ${
+                comicActivate ? "Header-span--activate" : ""
+              }`}
+            >
+              Buscar comics
+            </span>
+          </div>
         </div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
